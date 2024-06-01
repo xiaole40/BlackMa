741ea8e042f95329359678aa2f2a72687ede52fe
xiaole40 <1852183108@qq.com>
2024-06-01 22:02:04 +0800

    黑马下部分

---
+++ untitled/src/App.java
@@ -0,0 +1,17 @@
+import BlackMa.com.lele.puzzlegame.ui.GameJFrame;
+import BlackMa.com.lele.puzzlegame.ui.LoginJFrame;
+import BlackMa.com.lele.puzzlegame.ui.RegisterJFrame;
+
+public class App {
+    public static void main(String[] args) {
+        //表示程序的启动入口
+
+        //如果想要打开哪个界面就创建谁的对象
+        new GameJFrame();
+
+//        new LoginJFrame();
+
+//        new RegisterJFrame();
+
+    }
+}
