e415a0921d93810d705d5b710b86d98e760d1228
xiaole40 <1852183108@qq.com>
2024-07-19 14:05:03 +0800

    mythread多线程

---
+++ mythreadpool/threadpool1/MyRunnable.java
@@ -0,0 +1,10 @@
+package BlackMaNext.com.lele.mythreadpool.threadpool1;
+
+public class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(Thread.currentThread().getName() + "---" + i);
+        }
+    }
+}
